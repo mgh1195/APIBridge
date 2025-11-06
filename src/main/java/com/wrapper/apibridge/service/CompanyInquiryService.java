34d5d4ca3cce8e8a6bd9559d0068e17af08fcfbc
mahdi <m.m.rajabi.84@gmail.com>
2025-11-06 15:57:17 +0330

    feat: add CompanyInquiryService

---
+++ src/main/java/com/wrapper/apibridge/service/CompanyInquiryService.java
@@ -0,0 +1,86 @@
+package com.wrapper.apibridge.service;
+
+import com.wrapper.apibridge.service.dto.FinnotechResponse;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class CompanyInquiryService {
+    private final RestClient finnotechRestClient;
+    private final FinnotechTokenService finnotechTokenService;
+    private final String clientId;
+
+    public CompanyInquiryService(
+            RestClient finnotechRestClient,
+            FinnotechTokenService finnotechTokenService,
+            @Value("${app.finnotech.client-id}")
+            String clientId
+    ) {
+        this.finnotechRestClient = finnotechRestClient;
+        this.finnotechTokenService = finnotechTokenService;
+        this.clientId = clientId;
+    }
+
+    @Data
+    public static class CompanyInfoResponse {
+        private String companyId;
+        private String registrationNo;
+        private String title;
+        private String registrationDate;
+        private String capital;
+        private String address;
+        private String postalCode;
+        private String taxNumber;
+        private Double lat;
+        private Double lng;
+        private String website;
+        private String tel;
+        private String fax;
+        private String email;
+        private String status;
+        private String edareKol;
+        private String vahedSabti;
+        private String lastUpdate;
+        private String registrationTypeId;
+        private String registrationTypeTitle;
+        private String persianRegistrationDate;
+        private String knowledgeBasedState;
+        private String knowledgeBasedCategory;
+        private String knowledgeBasedConfirmationDate;
+    }
+
+    public CompanyInfoResponse inquiry(String companyId) {
+        return inquiry(companyId, null);
+    }
+
+    public CompanyInfoResponse inquiry(String companyId, String trackId) {
+
+        assert companyId != null;
+
+        StringBuilder requestUri = new StringBuilder(String.format(
+                "/kyb/v2/clients/%s/companyInfo?companyId=%s",
+                clientId,
+                companyId
+        ));
+
+        if (trackId != null) {
+            requestUri.append("&trackId=").append(trackId);
+        }
+
+        FinnotechResponse<CompanyInfoResponse> response = finnotechRestClient.get()
+                .uri(requestUri.toString())
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + finnotechTokenService.getToken())
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {
+                });
+
+        assert response != null;
+        assert response.getResult() != null;
+
+        return response.getResult();
+    }
+}
